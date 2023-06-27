e1fb4e059169c52c379941886e10efc507190181
NotFluent <satya9.chalumuri@gmail.com>
2023-06-27 21:33:31 +0530

    Corrected version

--- 373. Find K Pairs with Smallest Sums.cpp
+++ 373. Find K Pairs with Smallest Sums.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) 
-    {
-        priority_queue<pair<int,pair<int,int>>> pq;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>>ans;
+        priority_queue<pair<int,pair<int,int>>>pq;
 
-        for(int i = 0;i<nums1.size();i++)
-        {
-            for(int j = 0;j<nums2.size();j++)
-            {
-                int sum = nums1[i] + nums2[j];
-                if(pq.size()<k)
-                {
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                }
-                else if(sum<pq.top().first)
-                {
+
+
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                int sum=nums1[i]+nums2[j];
+                
+                if(pq.size()<k) pq.push({sum,{nums1[i],nums2[j]}});
+
+                else if(sum<pq.top().first){
                     pq.pop();
                     pq.push({sum,{nums1[i],nums2[j]}});
                 }
+                else{
+                    break;
+                }
             }
         }
-
-        vector<vector<int>> ans;
-        while(!pq.empty())
-        {
+        while(!pq.empty()){
+            
             ans.push_back({pq.top().second.first,pq.top().second.second});
             pq.pop();
         }
